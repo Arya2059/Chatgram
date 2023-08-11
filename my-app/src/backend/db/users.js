fe1ca2768fb94d2cee4fe5b714537f9b68b73e9d
Arya <119836600+Arya2059@users.noreply.github.com>
2023-08-12 02:18:12 +0530

    Update users.js

--- my-app/src/backend/db/users.js
+++ my-app/src/backend/db/users.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
 /**
  * User Database can be added here.
@@ -7,12 +6,42 @@ import { formatDate } from "../utils/authUtils";
 
 export const users = [
   {
-    _id: uuid(),
+    _id: "1",
     firstName: "Adarsh",
     lastName: "Balika",
     username: "adarshbalika",
-    password: "adarshBalika123",
+    password: "AdarshBalika123",
+    bio: "Reverse Engineer, CyberSecurity Enthusiast",
+    bookmarks: [],
+    avatarUrl: "",
+    website: "https://example.com",
     createdAt: formatDate(),
-    updatedAt: formatDate(),
+    updatedAt: formatDate()
   },
+  {
+    _id: "2",
+    firstName: "Arya",
+    lastName: "Shah",
+    username: "Arya009",
+    password: "ArSh@09",
+    bio: "Bug Hunter, CyberSec Professional",
+    bookmarks: [],
+    avatarUrl: "",
+    website: "https://arya-stellar.netlify.app",
+    createdAt: new Date("2023-06-24T10:30:00"),
+    updatedAt: new Date("2023-06-24T10:30:00")
+  },
+  {
+    _id: "3",
+    firstName: "Devansh",
+    lastName: "Patel",
+    username: "DevaP2001",
+    password: "DePa@01",
+    bio: "Cyber Security Aficionado, Network & Web Application Pentester",
+    bookmarks: [],
+    avatarUrl: "",
+    website: "https://evil.com",
+    createdAt: new Date("2023-06-24T11:45:00"),
+    updatedAt: new Date("2023-06-24T11:45:00")
+  }
 ];
