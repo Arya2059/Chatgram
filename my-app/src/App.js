e0dd0bc4db6e81afd843f02fa055701b0e8d27fd
Arya <119836600+Arya2059@users.noreply.github.com>
2023-08-12 02:07:05 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,82 @@
-import "./App.css";
-import logo from "./logo.png";
+import React, { useEffect } from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './styles/theme';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './components/rootLayout/RootLayout';
+import { PrivateRoute } from './components/auth/PrivatateRoute';
+import HomePage from './pages/HomePage';
+import ExplorePage from './pages/ExplorePage';
+import BookmarksPage from './pages/BookmarksPage';
+import LikedPage from './pages/LikedPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import PostDetailPage from './pages/PostDetailPage';
+import UserProfilePage from './pages/UserProfilePage';
+import ErrorPage from './pages/ErrorPage';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
+import { getAllUsers } from './slices/userSlice';
+import { useDispatch } from 'react-redux';
+import { getAllPosts } from './slices/postsSlice';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <ErrorPage />,
+    element: (
+      <PrivateRoute>
+        <RootLayout />
+      </PrivateRoute>
+    ),
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/explore',
+        element: <ExplorePage />,
+      },
+      {
+        path: '/bookmarks',
+        element: <BookmarksPage />,
+      },
+      {
+        path: '/liked',
+        element: <LikedPage />,
+      },
+      {
+        path: '/post/:id',
+        element: <PostDetailPage />,
+      },
+      {
+        path: '/profile/:username',
+        element: <UserProfilePage />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/signup',
+    element: <SignupPage />,
+  },
+]);
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllUsers());
+    dispatch(getAllPosts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="mockBee logo" width="180" height="180" />
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-        <p className="brand-description">
-          Get started by editing <code>src/App.js</code>
-        </p>
-        <div className="links">
-          <a
-            href="https://mockbee.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Explore mockBee
-          </a>
-          <a
-            href="https://mockbee.netlify.app/docs/api/introduction"
-            target="_blank"
-            rel="noreferrer"
-          >
-            API Documentation
-          </a>
-          <a
-            href="https://github.com/neogcamp/mockBee"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Contribute
-          </a>
-        </div>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   );
 }
 
